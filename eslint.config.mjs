8f2039e2bdb3bba974d65749d3789ed90296341e
dollarwolv <dollarwolv@gmail.com>
2025-07-22 15:21:58 +0200

    Configure ESLint to add a line above comments

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,27 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
-    plugins: { js },
+    plugins: { js, stylistic },
     extends: ["js/recommended"],
     languageOptions: {
       globals: {
         ...globals.node,
       },
+    },
+    rules: {
+      "stylistic/lines-around-comment": [
+        "error",
+        {
+          beforeLineComment: true,
+          allowBlockStart: true,
+          allowObjectStart: true,
+        },
+      ],
     },
   },
   {
